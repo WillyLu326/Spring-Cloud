0f6f8fccfc9d11b44b0656c21fbee4acf1f6d0be
WillyLu326 <luzh0326@gmail.com>
2017-07-24 22:48:08 -0400

    create eureka client

---
+++ spring-cloud-02-provider/src/main/java/com/example/demo/SpringCloud02ProviderApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud02ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloud02ProviderApplication.class, args);
+	}
+}
